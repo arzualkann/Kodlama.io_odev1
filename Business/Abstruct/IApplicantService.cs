f1d10bdbda4ce56066004637c31c8845e62528e4
arzualkann <104686755+arzualkann@users.noreply.github.com>
2024-02-12 03:02:27 +0300

    Add files via upload

---
+++ Business/Abstruct/IApplicantService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstruct;
+/**interface kelime anlamı arayüz.İçersinde sadece metot imasazı tutar.
+interfaceler belli metot imzalarını barındırırlar,birbirinin alternatifi olan sistemlerin farklı
+implementasyon yapmalarını sağlarlar.İnterfaceler newlenemez ama referans tutucudurlar.
+**/
+
+public interface IApplicantService
+{
+    void ApplyForMask(Person person);
+    List<Person> GetList();
+    bool CheckPerson(Person person);
+    
+}
